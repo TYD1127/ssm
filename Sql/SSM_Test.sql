8011ca7e32f2b834b0cc576440c53024d71b23a4
TYD1127 <2677243689@qq.com>
2020-09-29 17:50:30 +0800

    上传数据库脚本

---
+++ Sql/SSM_Test.sql
@@ -0,0 +1,53 @@
+/*
+SQLyog Enterprise v12.08 (64 bit)
+MySQL - 5.5.28 : Database - ssmbuild
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ssmbuild` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `ssmbuild`;
+
+/*Table structure for table `books` */
+
+DROP TABLE IF EXISTS `books`;
+
+CREATE TABLE `books` (
+  `bookID` int(10) NOT NULL AUTO_INCREMENT COMMENT '书id',
+  `bookName` varchar(100) NOT NULL COMMENT '书名',
+  `bookCounts` int(11) NOT NULL COMMENT '数量',
+  `detail` varchar(200) NOT NULL COMMENT '描述',
+  KEY `bookID` (`bookID`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+/*Data for the table `books` */
+
+insert  into `books`(`bookID`,`bookName`,`bookCounts`,`detail`) values (1,'Java',221,'从入门到放弃'),(2,'MySQL',1,'从删库到跑路'),(3,'Linux',5,'从进门到进牢');
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `uid` int(100) NOT NULL AUTO_INCREMENT,
+  `uname` varchar(10) NOT NULL,
+  `upwd` varchar(10) NOT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+insert  into `user`(`uid`,`uname`,`upwd`) values (1,'admin','root');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
